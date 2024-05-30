0c3cbeaaa7f06a34100f190e0015c4eea03f349f
Constantin Piber <cp.piber@gmail.com>
2024-05-31 00:13:24 +0200

    [DEB] Add hyprlang

---
+++ hyprlang/debian/upstream/metadata.ex
@@ -0,0 +1,10 @@
+# Example file for upstream/metadata.
+# See https://wiki.debian.org/UpstreamMetadata for more info/fields.
+# Below an example based on a github project.
+
+Bug-Database: https://github.com/hyprwm/hyprlang/issues
+Bug-Submit: https://github.com/hyprwm/hyprlang/issues/new
+Changelog: https://github.com/hyprwm/hyprlang/releases
+Documentation: https://hyprland.org/hyprlang
+Repository-Browse: https://github.com/hyprwm/hyprlang
+Repository: https://github.com/hyprwm/hyprlang.git
