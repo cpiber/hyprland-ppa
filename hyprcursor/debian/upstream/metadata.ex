3567b80106a4c37ab17c5bf585fb30a27210d44f
Constantin Piber <cp.piber@gmail.com>
2024-05-31 00:54:50 +0200

    [DEB] Add hyprcursor

---
+++ hyprcursor/debian/upstream/metadata.ex
@@ -0,0 +1,10 @@
+# Example file for upstream/metadata.
+# See https://wiki.debian.org/UpstreamMetadata for more info/fields.
+# Below an example based on a github project.
+
+Bug-Database: https://github.com/hyprwm/hyprcursor/issues
+Bug-Submit: https://github.com/hyprwm/hyprcursor/issues/new
+Changelog: https://github.com/hyprwm/hyprcursor/releases
+Documentation: https://wiki.hyprland.org
+Repository-Browse: https://github.com/hyprwm/hyprcursor
+Repository: https://github.com/hyprwm/hyprcursor.git
