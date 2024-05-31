041ba417f2bdce7172099585bf5b67ac9c7ec208
Constantin Piber <cp.piber@gmail.com>
2024-05-31 02:17:50 +0200

    [DEB] Add xcb-errors

---
+++ libxcb-errors/debian/upstream/metadata.ex
@@ -0,0 +1,10 @@
+# Example file for upstream/metadata.
+# See https://wiki.debian.org/UpstreamMetadata for more info/fields.
+# Below an example based on a github project.
+
+# Bug-Database: https://github.com/<user>/libxcb-errors/issues
+# Bug-Submit: https://github.com/<user>/libxcb-errors/issues/new
+# Changelog: https://github.com/<user>/libxcb-errors/blob/master/CHANGES
+# Documentation: https://github.com/<user>/libxcb-errors/wiki
+# Repository-Browse: https://github.com/<user>/libxcb-errors
+# Repository: https://github.com/<user>/libxcb-errors.git
