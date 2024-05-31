1e29906e0f4eaad6687988434f2b2e06e6dd6761
Constantin Piber <cp.piber@gmail.com>
2024-05-31 09:34:17 +0200

    [DEB] Add xdg-desktop-portal-hyprland

---
+++ xdg-desktop-portal-hyprland/debian/upstream/metadata.ex
@@ -0,0 +1,10 @@
+# Example file for upstream/metadata.
+# See https://wiki.debian.org/UpstreamMetadata for more info/fields.
+# Below an example based on a github project.
+
+Bug-Database: https://github.com/hyprwm/xdg-desktop-portal-hyprland/issues
+Bug-Submit: https://github.com/hyprwm/xdg-desktop-portal-hyprland/issues/new
+Changelog: https://github.com/hyprwm/xdg-desktop-portal-hyprland/releases
+Documentation: https://wiki.hyprland.org
+Repository-Browse: https://github.com/hyprwm/xdg-desktop-portal-hyprland
+Repository: https://github.com/hyprwm/xdg-desktop-portal-hyprland.git
