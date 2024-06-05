963ad96be0ff509d618f56ac07c86d9c33bbac5a
Constantin Piber <cp.piber@gmail.com>
2024-06-05 21:56:42 +0200

    [DEB] Add hyprland plugin dependecy meta package
    Should help with building plugins

---
+++ hyprland-plugin-deps/debian/upstream/metadata.ex
@@ -0,0 +1,10 @@
+# Example file for upstream/metadata.
+# See https://wiki.debian.org/UpstreamMetadata for more info/fields.
+# Below an example based on a github project.
+
+# Bug-Database: https://github.com/<user>/hyprland-plugin-deps/issues
+# Bug-Submit: https://github.com/<user>/hyprland-plugin-deps/issues/new
+# Changelog: https://github.com/<user>/hyprland-plugin-deps/blob/master/CHANGES
+# Documentation: https://github.com/<user>/hyprland-plugin-deps/wiki
+# Repository-Browse: https://github.com/<user>/hyprland-plugin-deps
+# Repository: https://github.com/<user>/hyprland-plugin-deps.git
