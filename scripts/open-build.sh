1cc42cdc550581b573c1c1382601f60e1b454003
Constantin Piber <cp.piber@gmail.com>
2024-06-05 21:53:11 +0200

    Scripts: also create orig.tar.xz for convenience

--- scripts/open-build.sh
+++ scripts/open-build.sh
@@ -7,7 +7,14 @@ fi
 
 set -xe
 . "`dirname "$0"`/prepare-build.sh"
+cd "$buildfolder"
+package="`dpkg-parsechangelog --show-field Source`"
+upstreamver="`dpkg-parsechangelog --show-field Version | sed 's/-[^-]\+//'`"
+echo "## Creating orig.tar.xz, this may take a while"
+# NOTE: -s for single is fine since we're not creating the debian/ folder
+dh_make --createorig -p "${package}_${upstreamver}" -s -y || :
+
+echo "## Opening shell, run your build command, e.g. pdebuild"
 shell="`getent passwd $LOGNAME | cut -d: -f7`"
 shell="${shell:-/bin/sh}"
-cd "$buildfolder"
 $shell
