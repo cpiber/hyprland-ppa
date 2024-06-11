72780aafa0ad88a832be4cdcd6bbb0bd0df63efc
Constantin Piber <cp.piber@gmail.com>
2024-06-11 12:41:22 +0200

    update.sh support tag-based update

--- scripts/update.sh
+++ scripts/update.sh
@@ -10,10 +10,29 @@ reporoot="`cd "$(dirname "$0")/.." && pwd`"
 
 project="$1"
 sourcefolder="$reporoot/$project/source"
+case "$project" in
+  hyprland)
+    type="main"
+    ;;
+  *)
+    type="tag"
+    ;;
+esac
+
 cd "$sourcefolder"
 curhead="`git rev-parse HEAD`"
-# for now only standard pull, no tags
-git pull --quiet --rebase
+if [ "$type" = "main" ]; then
+  git checkout main
+  git pull --quiet --rebase
+elif [ "$type" = "tag" ]; then
+  git fetch --tags
+  tag="`git describe --tags origin/main`"
+  git checkout "$tag"
+  tag="`echo "$tag" | sed 's/^v//'`"
+else
+  echo "Invalid type $type"
+  exit 1
+fi
 git submodule update --init --recursive
 newhead="`git rev-parse HEAD`"
 shorthead="`git rev-parse --short HEAD`"
@@ -23,10 +42,15 @@ if [ "$curhead" = "$newhead" ]; then
 fi
 changes="`git log --pretty="%h %s" $curhead..$newhead`"
 cd ..
-curver="`dpkg-parsechangelog --show-field Version | sed 's/+git.*//'`"
 dist="`dpkg-parsechangelog --show-field Distribution`"
-newver="$curver+git`date "+%Y%m%d%H%M"`-$shorthead"
-dch -v "$newver" -D "$dist" "Update to main $shorthead"
+if [ "$type" = "main" ]; then
+  curver="`dpkg-parsechangelog --show-field Version | sed 's/+git.*//'`"
+  newver="$curver+git`date "+%Y%m%d%H%M"`-$shorthead"
+  dch -v "$newver" -D "$dist" "Update to main $shorthead"
+else
+  newver="$tag-1ppa1"
+  dch -v "$newver" -D "$dist" "Update to release $tag"
+fi
 echo "$changes" |
   while IFS= read -r line; do
     dch -a "$line"
