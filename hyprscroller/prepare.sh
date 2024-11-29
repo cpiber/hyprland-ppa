69793116f7fed49f7e17b9ba80c403fc81c2cd65
Constantin Piber <cp.piber@gmail.com>
2024-11-29 12:28:36 +0100

    [DEB] hyprscroller: Update commit
    Also share debian/rules with hyprland-plugins

--- hyprscroller/prepare.sh
+++ hyprscroller/prepare.sh
@@ -5,3 +5,8 @@ cd "$sourcefolder"
 
 : Videos are only for the README
 rm -rf "$buildfolder/videos"
+
+: Get rules from hyprland-plugins
+cd "$buildfolder"
+sed 's/^NAME ?=.*$/NAME ?= hyprscroller/' "$sourcefolder/../../hyprland-plugins/debian/rules" > debian/rules
+chmod 0775 debian/rules
