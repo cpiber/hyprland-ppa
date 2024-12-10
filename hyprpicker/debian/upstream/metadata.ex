101977a707a5590a900ff8972aea7e89a2a75f89
Constantin Piber <cp.piber@gmail.com>
2024-12-10 23:33:39 +0100

    [DEB] hyprpicker: Introduce package

---
+++ hyprpicker/debian/upstream/metadata.ex
@@ -0,0 +1,10 @@
+# Example file for upstream/metadata.
+# See https://wiki.debian.org/UpstreamMetadata for more info/fields.
+# Below an example based on a github project.
+
+# Bug-Database: https://github.com/<user>/hyprpicker/issues
+# Bug-Submit: https://github.com/<user>/hyprpicker/issues/new
+# Changelog: https://github.com/<user>/hyprpicker/blob/master/CHANGES
+# Documentation: https://github.com/<user>/hyprpicker/wiki
+# Repository-Browse: https://github.com/<user>/hyprpicker
+# Repository: https://github.com/<user>/hyprpicker.git
