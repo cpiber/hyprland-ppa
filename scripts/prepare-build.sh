df068da7f5661c8ef8864a17e7719b86a30f0dc6
Constantin Piber <cp.piber@gmail.com>
2024-12-28 13:05:36 +0100

    Handle debian trixie

--- scripts/prepare-build.sh
+++ scripts/prepare-build.sh
@@ -19,5 +19,10 @@ rm -rf "$buildfolder"
 cp -ra "$debianfolder" "$buildfolder"
 if [ $# -gt 0 ]; then
   dch -c "$buildfolder/debian/changelog" -l "~1$1" -D "$1" "Rebuild for $1"
+  if [ "$1" = "trixie" ]; then
+    : Debian does not need this patch
+    rm -r "$buildfolder/debian/patches/01-patch-cms" || :
+    sed -i '/^01-patch-cms$/d' "$buildfolder/debian/patches/series" || :
+  fi
 fi
 [ -f "$reporoot/$project/prepare.sh" ] && . "$reporoot/$project/prepare.sh" || :
