d054dd675f106555c2e0737fbd7507b59b82ed3a
Vladimir-csp <4061903+Vladimir-csp@users.noreply.github.com>
2025-04-27 01:45:15 +0300

    Fallback to hyprland-stable for assets

--- hyprland-data/prepare.sh
+++ hyprland-data/prepare.sh
@@ -3,6 +3,13 @@
 set -xe
 cd "$sourcefolder"
 
-: Copy install assets from unstable
+: Copy install assets from hyprland submodule, fallback to hyprland-stable
 [ -d "$buildfolder/assets" ] || mkdir -p "$buildfolder/assets"
-cp -ra ../../hyprland/source/assets/install/*.png "$buildfolder/assets"
+if [ -d ../../hyprland/source/assets/install/ ]; then
+  cp -ra ../../hyprland/source/assets/install/*.png "$buildfolder/assets"
+elif [ -d ../../hyprland-stable/source/assets/install/ ]; then
+  cp -ra ../../hyprland-stable/source/assets/install/*.png "$buildfolder/assets"
+else
+  echo "No assets source: ../../hyprland/source/assets/install/, ../../hyprland-stable/source/assets/install/" >&2
+  exit 1
+fi
