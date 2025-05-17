7016dc1eac4f780fd84990ce628174fdcd03df47
Constantin Piber <cp.piber@gmail.com>
2025-05-17 15:44:04 +0200

    [DEB] hyprland-qtutils: Include svg support on jammy
    Was missed because the dev package is stupidly named on noble, but
    regular package is the same

--- hyprland-qtutils/jammy.sh
+++ hyprland-qtutils/jammy.sh
@@ -17,12 +17,12 @@ patch -p1 <<- "EOF"
  Standards-Version: 4.6.2
  Homepage: https://hyprland.org
  Vcs-Browser: https://github.com/hyprwm/hyprland-qtutils
-@@ -33,6 +36,5 @@
+@@ -33,7 +36,6 @@
   qml6-module-qtquick-layouts,
   qml6-module-qtquick-templates,
   qml6-module-qtqml-workerscript,
 - hyprland-qt-support0,
-- libqt6svg6,
-+ qt6-qpa-plugins,
+  qt6-qpa-plugins,
+  libqt6svg6,
  Description: Some qt/qml utilities that might be used by hypr apps
 EOF
