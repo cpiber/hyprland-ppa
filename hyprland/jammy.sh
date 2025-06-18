d5a2deca50d16bd3e2a9db6c696bb0d31fadad0b
Constantin Piber <cp.piber@gmail.com>
2025-06-18 20:24:34 +0200

    Partially Revert "[DEB] hyprland (unstable): Update commit"
    This reverts commit ca43c3b7d06eaf1ec1b32ce9737dab83944294bf.
    
    Using gcc-15 is not possible due to conflicts via libstdc++6
    
    Also moves libinput patch to jammy only

--- hyprland/jammy.sh
+++ hyprland/jammy.sh
@@ -6,3 +6,4 @@ cd "$buildfolder"
 echo jammy-01-opengl.patch >> debian/patches/series
 echo jammy-02-dma.patch >> debian/patches/series
 echo jammy-03-cairo.patch >> debian/patches/series
+echo jammy-04-libinput.patch >> debian/patches/series
