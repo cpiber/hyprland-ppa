0c7f06afdc05748269f7c49f0de90b51da1c33dc
juliokele <>
2025-07-11 20:24:06 +0000

    jammy.sh: Remove libinput-dev version guard

--- hyprland/jammy.sh
+++ hyprland/jammy.sh
@@ -7,3 +7,16 @@ echo jammy-01-opengl.patch >> debian/patches/series
 echo jammy-02-dma.patch >> debian/patches/series
 echo jammy-03-cairo.patch >> debian/patches/series
 echo jammy-04-libinput.patch >> debian/patches/series
+patch -p1 <<- "EOF"
+-- a/debian/control
++++ b/debian/control
+@@ -23,7 +23,7 @@ Build-Depends:
+  libcairo2-dev,
+  libpango1.0-dev,
+  libdrm-dev,
++ libinput-dev,
+- libinput-dev (>=1.28),
+  libxcb-xfixes0-dev,
+  libxcb-icccm4-dev,
+  libxcb-composite0-dev,
+EOF
