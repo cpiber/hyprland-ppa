ee788f0e868144c5839bbda63f26fe6e57b6784a
Constantin Piber <cp.piber@gmail.com>
2025-07-11 21:32:12 +0200

    [Scripts] Automatically fetch previous orig package
    If uploading a patch version for an existing upstream version, we must
    use the exact same file due to checksums (tar includes timestamps which
    mean the same files don't produce the same checksum)

--- scripts/open-build.sh
+++ scripts/open-build.sh
@@ -10,9 +10,14 @@ set -xe
 cd "$buildfolder"
 package="`dpkg-parsechangelog --show-field Source`"
 upstreamver="`dpkg-parsechangelog --show-field Version | sed 's/-[^-]\+//'`"
-echo "## Creating orig.tar.xz, this may take a while"
-# NOTE: -s for single is fine since we're not creating the debian/ folder
-dh_make --createorig -p "${package}_${upstreamver}" -s -y || :
+oldpackage="`sed -n "s/^${package} (\(${upstreamver}[^)]*\)).*$/\1/p" debian/changelog | tail -n1`"
+if [ ! -f "../${package}_${upstreamver}.orig.tar.xz" ]; then
+  echo "## Creating orig.tar.xz, this may take a while"
+  if !(cd .. && wget "https://launchpad.net/~cppiber/+archive/ubuntu/hyprland/+sourcefiles/${package}/${oldpackage}/${package}_${upstreamver}.orig.tar.xz"); then
+    # NOTE: -s for single is fine since we're not creating the debian/ folder
+    dh_make --createorig -p "${package}_${upstreamver}" -s -y || :
+  fi
+fi
 
 while [ $# -gt 0 ] && [ "$1" != "--" ]; do
   shift
