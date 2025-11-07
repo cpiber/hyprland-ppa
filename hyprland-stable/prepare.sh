487ea7cd5f9f46db8efd91065f788c595c446815
Constantin Piber <cp.piber@gmail.com>
2025-11-07 23:19:27 +0100

    [DEB] hyprland (stable): Update to 0.52.0

--- hyprland-stable/prepare.sh
+++ hyprland-stable/prepare.sh
@@ -1,18 +1,1 @@
-#!/bin/bash
-
-set -xe
-cd "$sourcefolder"
-
-: Remove subprojects, we use direct dependencies
-rm -rf "$buildfolder/subprojects"
-
-: Remove assets, we package them separately
-rm --interactive=never "$buildfolder/assets/install/"*.png
-
-: Hardcode version from git in orig blob
-eval "`sed -n '/^[^ ]\+=/p' scripts/generateVersion.sh`"
-vars="`sed -n 's/^\([^ ]\+\)=.*$/\1/p' scripts/generateVersion.sh`"
-echo "$vars" |
-  while IFS= read -r line; do
-    sed -i "s/^$line=.*$/$line='${!line}'/" "$buildfolder/scripts/generateVersion.sh"
-  done
+../hyprland/prepare.sh
