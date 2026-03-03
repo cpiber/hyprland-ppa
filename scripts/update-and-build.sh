727572206cd477107688be46bc90d0b18fb715a1
Constantin Piber <cp.piber@gmail.com>
2026-03-03 22:31:49 +0100

    [Scripts] Disable building hyprscroller

--- scripts/update-and-build.sh
+++ scripts/update-and-build.sh
@@ -47,7 +47,7 @@ if [ $# -gt 0 ]; then
 
   if [ "$project" = "hyprland" ] && [ "${2:-}" != "-s" ]; then
     buildplugin "hyprland-plugins" "${2:-}"
-    buildplugin "hyprscroller" "${2:-}"
+    # buildplugin "hyprscroller" "${2:-}"
     buildplugin "hy3" "${2:-}"
     buildplugin "hyprspace" "${2:-}"
     buildplugin "hyprsplit" "${2:-}"
@@ -69,7 +69,7 @@ else
 
     if [ "$project" = "hyprland" ] && [ "$rc" -eq 0 ]; then
       buildplugin "hyprland-plugins" "${2:-}"
-      buildplugin "hyprscroller" "${2:-}"
+      # buildplugin "hyprscroller" "${2:-}"
       buildplugin "hy3" "${2:-}"
       buildplugin "hyprspace" "${2:-}"
       buildplugin "hyprsplit" "${2:-}"
