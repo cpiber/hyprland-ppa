1a93a2d3ca9fa107f5ca7b544a290ea2863d4f76
Constantin Piber <cp.piber@gmail.com>
2026-07-18 14:48:19 +0200

    [Scripts] Improve launchpad download handling
    Previous script would accept any error, including when the server is
    down

--- scripts/open-build.sh
+++ scripts/open-build.sh
@@ -12,10 +12,20 @@ package="`dpkg-parsechangelog --show-field Source`"
 upstreamver="`dpkg-parsechangelog --show-field Version | sed 's/-[^-]\+$//'`"
 oldpackage="`sed -n "s/^${package} (\(${upstreamver}[^)]*\)).*$/\1/p" debian/changelog | head -2 | tail -1`"
 if [ ! -f "../${package}_${upstreamver}.orig.tar.xz" ]; then
+  set +x
   echo "## Creating orig.tar.xz, this may take a while"
-  if !(cd .. && wget "https://launchpad.net/~cppiber/+archive/ubuntu/hyprland/+sourcefiles/${package}/${oldpackage}/${package}_${upstreamver}.orig.tar.xz"); then
+
+  exec 3>&1
+  res="$(cd .. && wget "https://launchpad.net/~cppiber/+archive/ubuntu/hyprland/+sourcefiles/${package}/${oldpackage}/${package}_${upstreamver}.orig.tar.xz" 3>&- 2>&1 | tee /dev/fd/3; exit ${PIPESTATUS[1]})"
+  rc=$?
+  exec 3>&-
+  set -x
+
+  if [ $rc -ne 0 ] && echo "$res" | grep "ERROR 404" >/dev/null; then
     # NOTE: -s for single is fine since we're not creating the debian/ folder
     dh_make --createorig -p "${package}_${upstreamver}" -s -y || :
+  elif [ $rc -ne 0 ]; then
+    exit $rc
   fi
 fi
 
